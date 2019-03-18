c54561c3f7052c4df2b1bc57feb9d19154b4b1f1
Marcus Mellor <marcus@mellor.com>
2019-03-18 16:54:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
-
+  result=""
+  counter=0
+  if array.count<=1 
+    result=array[0]
+  elsif array.count==2
+    result=array[0]
+    result << " and "
+    result << array[1]
+  else
+    while counter<array.count
+      counter+=1
+      result+= array[counter-1]
+      if counter<=array.count-2
+        result+=", "
+      elsif counter==array.count-1
+        result+=", and "
+      end
+    end
+  end
+  
+  result
+  
 end
