40af2fb367d3e3b3e239f4d18d3d5d72d0f6a999
Jason Bamford <47010074+jb-23@users.noreply.github.com>
2019-03-26 19:20:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
+
+
 def oxford_comma(array)
+
+  size = array.length
+
+  if size < 2
+    return array[0]
+  end
+
+  if size == 2
+    return array.join(" and ")
+  end
+
+  size -= 1             # avoid subtraction in loop
+  sentence = ""
+  
+  array.each.with_index do | clause, index |
+    sentence += ", " if index > 0
+    sentence += "and " if index == size
+    sentence += clause
+  end
+
+  return sentence
 
 end
