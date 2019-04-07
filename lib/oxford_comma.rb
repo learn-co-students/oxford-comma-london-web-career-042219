74a5b74f66fff39c50ba619a19595adf00272a2a
Aiden leeming <aidenjl193@gmail.com>
2019-04-07 20:03:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  string = ""
+  for i in 0..array.length - 1 do
+    string << array[i]
+    break if i == array.length - 1
+    string << (i == array.length - 2 ? "#{i == 0 ? "" : ","} and " : ", ")
+  end
+  string
 end
